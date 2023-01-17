9be7b8ba7a1cc626f18ec6a208c454295b781e4c
marcel <marcel.ggdargmail.com>
2023-01-17 13:32:20 +0100

    Mas idiomas

--- js/index.js
+++ js/index.js
@@ -21,16 +21,26 @@ async function changeLanguage(lang) {
   // console.log(document);
   // document.getElementById('').innerHTML='testing';
 
-  document.getElementById('access').innerHTML=data.access;
-  document.getElementById('firstName').innerHTML=data.firstName.textLabel;
-  document.getElementById('firstName').placeHolder=data.firstName.placeholder;
-  document.getElementById('lastName').innerHTML=data.lastname.textLabel;
-  document.getElementById('lastName').placeHolder=data.lastname.placeHolder;
-  document.getElementById('usuari').innerHTML=data.usuari.textLabel;
-  document.getElementById('usuari').placeHolder=data.usuari.placeHolder;
-  document.getElementById('mail').innerHTML=data.mail.textLabel;
-  document.getElementById('mail').placeHolder=data.mail.placeHolder;
-  document.getElementById('passwd').innerHTML=data.passwd.textLabel;
-  document.getElementById('passwd').placeHolder=data.passwd.placeHolder;
-  document.getElementById('btn').innerHTML=data.btn;
+  //LOGIN
+ if(document.getElementById('access')) document.getElementById('access').innerHTML=data.access;
+ if(document.getElementById('firstName')) document.getElementById('firstName').innerHTML=data.firstName.textLabel;
+ if(document.getElementById('firstName')) document.getElementById('firstName').placeHolder=data.firstName.placeholder;
+ if(document.getElementById('lastName')) document.getElementById('lastName').innerHTML=data.lastname.textLabel;
+ if(document.getElementById('lastName')) document.getElementById('lastName').placeHolder=data.lastname.placeHolder;
+ if(document.getElementById('usuari')) document.getElementById('usuari').innerHTML=data.usuari.textLabel;
+ if(document.getElementById('usuari')) document.getElementById('usuari').placeHolder=data.usuari.placeHolder;
+ if(document.getElementById('mail')) document.getElementById('mail').innerHTML=data.mail.textLabel;
+ if(document.getElementById('mail')) document.getElementById('mail').placeHolder=data.mail.placeHolder;
+ if(document.getElementById('passwd')) document.getElementById('passwd').innerHTML=data.passwd.textLabel;
+ if(document.getElementById('passwd')) document.getElementById('passwd').placeHolder=data.passwd.placeHolder;
+ if(document.getElementById('btn')) document.getElementById('btn').innerHTML=data.btn;
+
+  //INDEX
+  if(document.getElementById('linkInici')) document.getElementById('linkInici').innerHTML=data.linkInici;
+  if(document.getElementById('linkCompra')) document.getElementById('linkCompra').innerHTML=data.linkCompra;
+  if(document.getElementById('linkLogin')) document.getElementById('linkLogin').innerHTML=data.linkLogin;
+  if(document.getElementById('linkAbout')) document.getElementById('linkAbout').innerHTML=data.linkAbout;
+  //COMPRA
+
+
 }
